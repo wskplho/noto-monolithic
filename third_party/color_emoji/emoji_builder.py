d29392f3f441c7c5bc0749a7e857749998d30414
Roozbeh Pournader <roozbeh@google.com>
2014-10-03 12:36:26 -0700

    Update authors line in emoji_builder.py

--- third_party/color_emoji/emoji_builder.py
+++ third_party/color_emoji/emoji_builder.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Google Author(s): Behdad Esfahbod, Stuart Gill
+# Google Author(s): Behdad Esfahbod, Stuart Gill, Roozbeh Pournader
 #
 
 
